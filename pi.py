310925c4bc72ed45803dde56e40b30f080e03f79
ISS_RabbitEars <95940623+ISS-RabbitEars@users.noreply.github.com>
2021-12-12 20:35:44 -0800

    Add files via upload

---
+++ pi.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+from matplotlib import animation
+import random as rnd
+import math
+
+def setplot1(sx,sy):
+	ax=plt.gca()
+	ax.set_aspect(1)
+	plt.xlim([-sx,sx])
+	plt.ylim([-sy,sy])
+	ax.set_facecolor('xkcd:black')
+
+def setplot2(sx1,sx2,sy1,sy2):
+	ax=plt.gca()
+	ax.set_aspect(1)
+	plt.xlim([sx1,sx2])
+	plt.ylim([sy1,sy2])
+	ax.set_facecolor('xkcd:black')
+
+
+frps=60
+sec=3*60
+
+fig, a=plt.subplots()
+nr=0
+nb=0
+per='%'
+
+def run(frame):
+	global nr,nb
+	x=rnd.random()
+	y=rnd.random()
+	if math.sqrt(x**2+y**2)<=1:
+		plt.plot([x],[y],'ro',markersize=1)
+		nr+=1
+	else:
+		plt.plot([x],[y],'bo',markersize=1)
+		nb+=1
+	pi=4*nr/(nr+nb)
+	plt.suptitle(r'$\pi \approx$ %f' % pi)
+	pe=100*abs(pi-math.pi)/math.pi
+	plt.title(per+'Error = %f' % pe)
+	setplot2(0,1,0,1)
+
+ani=animation.FuncAnimation(fig,run,frames=frps*sec)
+writervideo=animation.FFMpegWriter(fps=frps)
+ani.save('pi_est.mp4', writer=writervideo)
+plt.show()
